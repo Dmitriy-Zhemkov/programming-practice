471a7b7f997eab4fbb861d880eac4b81cbc110ea
Dmitriy-Zhemkov <45768725+Dmitriy-Zhemkov@users.noreply.github.com>
2021-09-23 01:38:29 +0300

    Practice_3_Modified (#3)
    * Practice_3_Modified
    
    * Practice_3
    
    * Practice_3

--- Practice/Practice_3/main.cpp
+++ Practice/Practice_3/main.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
+#include <tuple>
+#include <numeric>
 
 using namespace std;
 
-int GetRandomNumberInt(int min, int max) {
+auto GetRandomNumbers(int min, int max) {
     int num = min + rand()%(max - min +1);
-    return num;
-}
-float GetRandomNumberFloat(float min, float max) {
-    float flo =  min + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(max-min)));
-    return flo;
+    float flo =  float(0.0) + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(float(1.0)-float(0.0))));
+    return std::make_tuple(num, flo);
 }
 
 int main() {
-    int i;
-    float f;
-    int number;
     srand(time(NULL));
+    std::vector<int> vector_int;
+    std::vector<float> vector_float;
+    double sum = 0;
     for (int i=0; i < 100; i++) {
-        number = GetRandomNumberInt(-100, 100);
+        auto [number, f]= GetRandomNumbers(-100, 100);
         cout << "int number = " << number << endl;
-        f = GetRandomNumberFloat(0,1);
         cout << "float number = " << f << endl;
+        vector_int.push_back(number);
+        vector_float.push_back(f);
+        sum += number;
     }
+    float mean_int = std::accumulate(vector_int.begin(), vector_int.end(), 0.0) / vector_int.size();
+    float mean_float = std::accumulate(vector_float.begin(), vector_float.end(), 0.0) / vector_int.size();
+    cout << "mean_int = " << mean_int << endl << "mean_float = " << mean_float << endl;
+    auto disp_int = 0;
+    auto disp_float = 0.0;
+    for(int i = 0; i < vector_int.size(); i++ )
+    {
+        disp_int += ( vector_int[i] - mean_int ) * ( vector_int[i] - mean_int );
+        disp_float += ( vector_float[i] - mean_float ) * ( vector_float[i] - mean_float );
+    }
+    disp_int /= (vector_int.size()- 1 );
+    disp_float /= (vector_float.size()- 1);
+    cout << "dispersion_int = " << disp_int << endl << "dispersion_float = "  << disp_float << endl;
     return 0;
 }
